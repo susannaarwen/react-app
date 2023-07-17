5330916acbea25e3499083bdddb367c0bc95a3bd
susannaarwen <susannachapman@zoa.io>
2023-07-17 19:38:13 +0100

    add blog post

--- src/App.js
+++ src/App.js
@@ -69,7 +69,11 @@ function App() {
       </article>
 
       <article class = 'entry' id='day3'>
-      {/*<h2>Friday the 14th of July 2023</h2>*/}
+        <h2>Friday the 14th of July 2023</h2>
+        <p>Erm so today was a very very big and special day for me.</p>
+        <p>I built my first text-based adventure game in TypeScript!</p>
+        <p>It was based on a crisis that we had all been having on the apprenticeship, as the problem was so hard. So it was in honour of, and dedicated to, my friend Robert on the apprenticeship!</p>
+        <p>I can neither confirm nor deny that any Chat GPT was used - but even then it was still such a big achievement for me.</p>
       </article>
       </body>
       
