a46cc3df2ad152b158ed9a6d79c63dd7c33a7971
susannaarwen <susannachapman@zoa.io>
2023-07-20 18:34:53 +0100

    update new blog post

---
+++ src/components/Day7.js
@@ -0,0 +1,16 @@
+function Day7 () {
+    return (
+        <article class = 'entry' id='day7'>
+            <h2>Thursday the 20th of July 2023</h2>
+            <p>Today was a great day.</p>
+            <p>I did a lot of React. I spent most of the day obsessively listening to "Jon Batiste Interlude" on Lana del Rey's new album. I'm hooked. I also listened to other songs on repeat - oh no wait it was this one all day LOL. It's STUNNING.</p>
+            <p>What did I do today in React?</p>
+            <ul>
+                <li>I read over my instructor's 2 React snippets he sent me - and I watched his video he sent me on them, AMAZING.</li>
+                <li>I read over a new Senior Engineer's tips on React and I made a note of them as well.</li>
+                <li>I read over my bootcamp's intro to React on our SWE Learning Plans page. It was AMAZING OMG.</li>
+            </ul>
+        </article>
+     
+    )
+};
