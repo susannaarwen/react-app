892e04d7017c7afec655c2fab324bb181d751263
susannaarwen <susannachapman@zoa.io>
2023-07-20 18:53:00 +0100

    add picture

--- src/components/Day7.js
+++ src/components/Day7.js
@@ -1,9 +1,13 @@
+import lana1 from '../images/lana1.png';
+
+
 function Day7 () {
     return (
         <article class = 'entry' id='day7'>
             <h2>Thursday the 20th of July 2023</h2>
             <p>Today was a great day.</p>
             <p>I did a lot of React. I spent most of the day obsessively listening to "Jon Batiste Interlude" on Lana del Rey's new album. I'm hooked. I also listened to other songs on repeat - oh no wait it was this one all day LOL. It's STUNNING.</p>
+            <img src={lana1} id="lana1" alt="Album cover for Lana del Rey's 'Did you know that there's a tunnel under Ocean boulevard' album. Black and white Lana del Rey's face pouting with her chin in her hand and her hair tied in ribbons." />
             <p>What did I do today in React?</p>
             <ul>
                 <li>I read over my instructor's 2 React snippets he sent me - and I watched his video he sent me on them, AMAZING.</li>
